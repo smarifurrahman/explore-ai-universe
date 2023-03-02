53f88e55e17f9e80881f374c383f90fae13368c2
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-03-03 01:00:08 +0600

    data limit added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
-const loadData = async () => {
+const dataLimit = 6;
+const loadData = async (dataLimit) => {
     const url = 'https://openapi.programming-hero.com/api/ai/tools';
     const res = await fetch(url);
     const data = await res.json();
-    displayData(data.data.tools);
+    displayData(data.data.tools, dataLimit);
 }
 
-const displayData = (tools) => {
-    // console.log(tools);
+const displayData = (tools, dataLimit) => {
+    // console.log(tools.length);
     const cardsContainer = document.getElementById('cards-container');
+    const btnShowMore = document.getElementById('btn-see-more');
+    if (dataLimit && dataLimit < tools.length) {
+        tools = tools.slice(0, dataLimit);
+        btnShowMore.classList.remove('hidden');
+    }
+    else {
+        btnShowMore.classList.add('hidden');
+    }
     tools.forEach(tool => {
         const card = document.createElement('div');
         card.innerHTML = `
         <!-- card -->
-        <div class="card w-96 bg-base-100 shadow-xl border">
+        <div class="card w-96 bg-base-100 shadow-xl border h-full">
             <figure class="px-7 pt-7">
                 <img src="${tool.image}" alt="Shoes"
                     class="rounded-xl" />
@@ -25,8 +34,8 @@ const displayData = (tools) => {
                 <hr class="w-full">
                 <div class="card-actions w-full flex justify-between items-center mt-2">
                     <div>
-                        <h2 class="card-title mb-1.5">ChatGPT</h2>
-                        <p><i class="fa-solid fa-calendar-days mr-1 text-secondary"></i> 11/01/2022</p>
+                        <h2 class="card-title mb-1.5">${tool.name}</h2>
+                        <p><i class="fa-solid fa-calendar-days mr-1 text-secondary"></i> ${tool.published_in}</p>
                     </div>
                     <div>
                         <i class="fa-solid fa-arrow-right p-4 rounded-full text-primary bg-bgPrimary"></i>
@@ -49,4 +58,18 @@ const showFeatures = (features, id) => {
     });
 }
 
-loadData();
+const emptyDataContainer = () => {
+    const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.innerHTML = '';
+}
+
+const processData = (dataLimit = false) => {
+    emptyDataContainer();
+    loadData(dataLimit);
+}
+
+document.getElementById('btn-see-more').addEventListener('click', function () {
+    processData();
+});
+
+processData(dataLimit);
